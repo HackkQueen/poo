b57d8867c5413bc8ce5958f6fedac24379cd54fe
Mafe Cubides <eviqueenchan@gmail.com>
2023-05-25 01:52:46 -0500

    feat: :sparkles: Start of project structuring
    Start structuring the project html, css and javascript sections (part 1).

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
+/*
+TODO: VALIDACION DE FORMULARIO
+*/
 (() => {
-    'use strict'
-    const forms = document.querySelectorAll('.needs-validation')
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+  'use strict'
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+/*
+TODO: PRIMERA PARTE DEL EJERCICIO SECCION PERSONA
+*/
 class Persona{
     nombre
     edad
@@ -22,9 +32,45 @@ class Persona{
         this.sexo=sexo;
     }
     saludar(){
-        return `Hola soy ${this.nombre} y tengo ${this.edad} años de edad y soy ${this.sexo}`;
+        return `Hola soy ${this.nombre} y tengo ${this.edad} años de edad y mi sexo es ${this.sexo}`;
+    }
+    static esMayorDeEdad(edad){
+      if (edad >= 18){
+          return `<p>Es MAYOR de edad</p>`
+      } else {
+          return `<p>Es MENOR de edad</p>`
+      }
+    }
+    get getEdad(){
+      return this.edad
     }
 }
+
+let persona1=new Persona({nombre:"Maria Cubides",edad:20,sexo:"Femenino"});
+console.log(persona1.saludar());
+document.querySelector('.res1').innerHTML = persona1.saludar()
+
+/*
+TODO: SEGUNDA PARTE DEL EJERCICIO SECCION ESTUDIANTE
+*/
+class Estudiante extends Persona{
+  carrera
+  constructor({nombre,edad,sexo,carrera}){
+    super({ nombre,edad,sexo });
+    this.carrera = carrera;
+  }
+  estudiar(){
+    return `La carrera que el estudiante ${this.nombre} esta estudiando es ${this.carrera}`;
+  }
+}
+
+let estudiante1=new Estudiante({nombre:"Maria Cubides",edad:225,sexo:"Alienigena",carrera:"Robot"});
+console.log(estudiante1.saludar());
+console.log(estudiante1.estudiar());
+document.querySelector('.res3').innerHTML = estudiante1.saludar() + ", teniendo encuenta eso:  " + estudiante1.estudiar();
+/*
+TODO:  FORMULARIO
+*/
 
 const formulario=document.getElementById("formPersona");
 formulario.addEventListener("submit",function(e){
@@ -33,4 +79,14 @@ formulario.addEventListener("submit",function(e){
     const persona=new Persona(data)
     persona.saludar();
     console.log(persona.saludar());
+    document.querySelector('.resto').innerHTML = persona.saludar()
+
+    const carrera = e.target.carrera.value;
+    const est = new Estudiante({ nombre: persona.nombre, carrera });
+    console.log(est.estudiar());
+    document.querySelector('.res2').innerHTML = est.estudiar();
+
+    const esMayorDeEdad = Persona.esMayorDeEdad(persona.edad);
+    console.log(`La Persona ${persona1.nombre} es: ${esMayorDeEdad}`);
+    document.querySelector('.res4').innerHTML = `La Persona ${persona1.nombre} es: ${esMayorDeEdad}`;
 })
